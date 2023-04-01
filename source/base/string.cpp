6d061b5df762d31337ad143208786650fba76499
Torsten Mayer-Guerr <mayer-guerr@tugraz.at>
2023-04-01 10:22:45 +0200

    string: trim: remove also '\r' (Carriage Return).

--- source/base/string.cpp
+++ source/base/string.cpp
@@ -39,10 +39,10 @@ std::string String::trim(const std::string &str)
 {
   try
   {
-    auto start = str.find_first_not_of(" \t");
+    auto start = str.find_first_not_of(" \t\r");
     if(start == std::string::npos)
       return "";
-    auto end = str.find_last_not_of(" \t");
+    auto end = str.find_last_not_of(" \t\r");
     return str.substr(start, end-start+1);
   }
   catch(std::exception &e)
@@ -57,7 +57,7 @@ std::string String::trimLeft(const std::string &str)
 {
   try
   {
-    auto start = str.find_first_not_of(" \t");
+    auto start = str.find_first_not_of(" \t\r");
     if(start == std::string::npos)
       return "";
     return str.substr(start);
@@ -74,7 +74,7 @@ std::string String::trimRight(const std::string &str)
 {
   try
   {
-    auto end = str.find_last_not_of(" \t");
+    auto end = str.find_last_not_of(" \t\r");
     if(end == std::string::npos)
       return "";
     return str.substr(0, end+1);
