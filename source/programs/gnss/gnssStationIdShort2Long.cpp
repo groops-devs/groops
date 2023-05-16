f095cca9e9c77363ad485407e1f46267cb19604c
Hauschild, André <andre.hauschild@dlr.de>
2023-05-16 10:29:15 +0200

    Compression of source code using readFileStringTable() and find_if()

--- source/programs/gnss/gnssStationIdShort2Long.cpp
+++ source/programs/gnss/gnssStationIdShort2Long.cpp
@@ -18,6 +18,7 @@ Create \reference{variables}{general.parser} containing long (9 char) site ID ba
 /***********************************************/
 
 #include "programs/program.h"
+#include "files/fileStringTable.h"
 
 #include <map>
 #include <string>
@@ -41,7 +42,8 @@ void GnssStationIdShort2Long::run(Config &config, Parallel::CommunicatorPtr /*co
 {
   try
   {
-    std::string site4char;
+
+   	std::string site4char;
     std::string site9char;
     FileName    fileNameSiteID;
 
@@ -52,33 +54,16 @@ void GnssStationIdShort2Long::run(Config &config, Parallel::CommunicatorPtr /*co
 
     logStatus<<"read site ID conversion file <"<<fileNameSiteID<<">"<<Log::endl;
 
-    std::string line;
-    std::map<std::string,std::string> siteIDs;
+    std::vector<std::vector<std::string>> siteIDs;
 
-    std::ifstream inp(fileNameSiteID.c_str());
-    if (inp.fail()) {
-      throw(Exception(fileNameSiteID.str()+" cannot open input file!"));
-    };
-    while(!inp.eof()){
+    readFileStringTable(fileNameSiteID, siteIDs);
 
-      getline(inp,line);
-      if (line.size()<14) continue;
-
-      std::string site4c = line.substr(0, 4);
-      std::string site9c = line.substr(5,14);
-
-      siteIDs[site4c] = site9c;
-
-      //logStatus <<"  found <" << site4c << "> -> <" << site9c << ">" << Log::endl;
-
-    };
-    inp.close();
-
-    if (siteIDs.find(site4char)==siteIDs.end())
+    auto iter = std::find_if(siteIDs.begin(), siteIDs.end(),
+                             [&](const auto &x){return x.size() && (x.at(0) == site4char);});
+    if(iter == siteIDs.end())
       throw(Exception(fileNameSiteID.str()+" contains no site ID <"+site4char+">"));
 
-    if(!site9char.empty())
-      addVariable(site9char, siteIDs.find(site4char)->second, config.getVarList());
+    addVariable(site9char, iter->at(1), config.getVarList());
 
   }
   catch(std::exception &e)
